cb8af3e0f930fa7cb1721f0f80221219d29623a5
msidiqh991 <msidiqh991@gmail.com>
2023-09-23 23:10:05 +0700

    feat: add data from local storage

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('title').value = '';
         document.getElementById('date').value = '';
 
+        updateTaskCount();
         document.dispatchEvent(new Event(RENDER_EVENT));
         saveData();
     }
@@ -41,33 +42,13 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    function saveData(){
-        if(isStorageExist()){
-            const parsed = JSON.stringify(todos);
-            localStorage.setItem(STORAGE_KEY, parsed);
-            document.dispatchEvent(new Event(SAVED_EVENT));
-        }
-    }
-
-    function isStorageExist() {
-        if(typeof(Storage) === undefined){
-            alert('Browser kamu tidak mendukung local storage');
-            return false;
-        }
-        return true;
-    }
-
-    document.addEventListener(SAVED_EVENT, function() {
-        console.log(localStorage.getItem(STORAGE_KEY));
-    })
-
     document.addEventListener(RENDER_EVENT, function () {
         const uncompletedTODOList = document.getElementById('todos');
         uncompletedTODOList.innerHTML = '';
        
         const completedTODOList = document.getElementById('completed-todos');
         completedTODOList.innerHTML = '';
-       
+
         for (const todoItem of todos) {
             const todoElement = makeTodo(todoItem);
             if (!todoItem.isCompleted)
@@ -75,7 +56,16 @@ document.addEventListener('DOMContentLoaded', function() {
             else
                 completedTODOList.append(todoElement);
         }
-      });
+    });
+
+    // Update jumlah list todo pada local storage
+    function updateTaskCount() {
+        const uncompletedCount = todos.filter(todo => !todo.isCompleted).length;
+        localStorage.setItem('uncompletedCount', uncompletedCount);
+
+        const taskValue = document.getElementById('taskValue');
+        taskValue.innerText = uncompletedCount;
+    }
 
     function addTaskToCompleted(todoId){
         const todoTarget = findTodo(todoId);
@@ -83,6 +73,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if(todoTarget == null) return;
 
         todoTarget.isCompleted = true;
+        updateTaskCount()
         document.dispatchEvent(new Event(RENDER_EVENT));
         saveData();
     }
@@ -102,6 +93,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (todoTarget === -1) return;
        
         todos.splice(todoTarget, 1);
+        updateTaskCount()
         document.dispatchEvent(new Event(RENDER_EVENT));
         saveData();
     }
@@ -121,6 +113,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (todoTarget == null) return;
        
         todoTarget.isCompleted = false;
+        updateTaskCount()
         document.dispatchEvent(new Event(RENDER_EVENT));
         saveData();
     }
@@ -170,4 +163,47 @@ document.addEventListener('DOMContentLoaded', function() {
         }    
         return container;
     }
-})
+
+    // Menyimpan data pada local Storage
+    function saveData(){
+        if(isStorageExist()){
+            const parsed = JSON.stringify(todos);
+            localStorage.setItem(STORAGE_KEY, parsed);
+            document.dispatchEvent(new Event(SAVED_EVENT));
+        }
+    }
+
+    // Validasi apakah browser mendukung local storage?
+    function isStorageExist() {
+        if(typeof(Storage) === undefined){
+            alert('Browser kamu tidak mendukung local storage');
+            return false;
+        }
+        return true;
+    }
+
+    // Mencari data pada local storage
+    function loadDataFromStorage(){
+        const serializedData = localStorage.getItem(STORAGE_KEY);
+        let data = JSON.parse(serializedData);
+
+        if(data !== null){
+            for(const todo of data){
+                todos.push(todo);
+            }
+        }
+
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    }
+
+    // Validasi pencarian data pada local storage
+    if(isStorageExist) {
+        loadDataFromStorage();
+    }
+
+    /* Memperbarui data pada storage secara langsung,
+    dan mengambil data dari local storage */
+    document.addEventListener(SAVED_EVENT, function() {
+        console.log(localStorage.getItem(STORAGE_KEY));
+    });
+});
